f8b3fc36f99bcb016c4273b70d8f35f64bc2685d
WSONG <46225024+WWEISONG@users.noreply.github.com>
2020-06-24 23:32:58 +1000

    add relationship between entities

--- backend/models.py
+++ backend/models.py
@@ -18,32 +18,37 @@ class Role(db.Model):
     __tablename__ = 'role'
     role_id = db.Column(db.Integer, primary_key=True)
     role_name = db.Column(db.String(20))
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
 class Opshop(db.Model):
     __tablename__ = 'opshop'
-    opshop_id = db.Column(db.Integer, primary_key=True)
+    opshop_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     opshop_name = db.Column(db.String(20), nullable=True)
     opshop_address = db.Column(db.String(100))
     staff_number = db.Column(db.Integer)
     opshop_status = db.Column(db.String(20))
+    users = db.relationship('User', backref='opshop', lazy='dynamic')
+    books = db.relationship('Book', backref='opshop', lazy='dynamic')
 
 class ItemType(db.Model):
     __tablename__ = 'itemtype'
     item_type_id = db.Column(db.Integer, primary_key=True)
     item_type_name = db.Column(db.String(20))
+    items = db.relationship('Book', backref='items', lazy='dynamic')
 
 class Order(db.Model):
     __tablename__ = 'order'
-    order_id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.String, primary_key=True)
     opshop_id = db.Column(db.Integer, db.ForeignKey('Opshop.opshop_id'), nullable=False)
     customer_address = db.Column(db.String(100))
-    order_date = db.Column(db.Date)
+    order_date = db.Column(db.DateTime)
     order_status = db.Column(db.String(20))
+    order_items = db.relationship('OrderItems', backref='order', lazy='dynamic')
 
 class OrderItems(db.Model):
     __tablename__ = 'orderitems'
-    order_id = db.Column(db.Integer, db.ForeignKey('Order.order_id'), nullable=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('book.book_id'), nullable=False)
+    order_id = db.Column(db.String, db.ForeignKey('Order.order_id'), primary_key=True, nullable=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('book.book_id'), primary_key=True, nullable=False)
     item_type_id = db.Column(db.Integer, db.ForeignKey('ItemType.item_type_+id'))
     quantity = db.Column(db.Integer)
     single_price = db.Column(db.Float)
