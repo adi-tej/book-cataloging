b8be4cbed7c15f0d7e0bd6febb2e392408de07c9
WSONG <46225024+WWEISONG@users.noreply.github.com>
2020-07-13 23:36:57 +1000

    two plans for notifications from ebay

--- backend/notifications.py
+++ backend/notifications.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
 from ebaysdk.trading import Connection
+from time import time, strftime, localtime
 
 from app import opshop_api, db
 from models import *
+from auth import token_required
 
 import json
 
@@ -16,3 +18,21 @@ notification_api = opshop_api.namespace(
     description="notifications management process"
 )
 
+@notification.route('/order/')
+class Notifications(Resource):
+    @notification_api.doc(description="retrive latest order in real time")
+    @token_required
+    def get(self):
+        api = Connection(config_file="ebay.yaml", domain="api.sandbox.ebay.com", debug=True)
+        past_time = time() - 10 * 60 * 60 - 1*60
+        past_time = strftime('%Y-%m-%d %H:%M:%S', localtime(past_time))
+        cur_time = time() - 10 * 60 * 60
+        cur_time = strftime('%Y-%m-%d %H:%M:%S', localtime(cur_time))
+        request_info = {
+            "CreateTimeFrom": past_time,
+            "CreateTimeTo": cur_time,
+            "IncludeFinalValueFee": True,
+            "OrderRole": "Seller",
+        }
+        # two plans
+        resp = api.execute("GetOrders", request_info)
