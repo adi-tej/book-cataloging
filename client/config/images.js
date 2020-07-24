9dd8e84f5d9b2643ba6005867809f93dab3de1a4
Natalie <52368074+Nataliezhong8@users.noreply.github.com>
2020-07-24 17:44:07 +1000

    update circex image

--- client/config/images.js
+++ client/config/images.js
@@ -1,6 +1,7 @@
 const images = {
 
-    logo: require('../assets/Rectangle614.png'),
+    // logo: require('../assets/Rectangle614.png'),
+    logo: require('../assets/circex.png'),
     bookCover: require('../assets/bookshelf.jpg')
 };
 
