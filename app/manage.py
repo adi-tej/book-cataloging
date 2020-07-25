30415e774cd5a63a21d11ce60da6fac89ac20bfe
宋伟 <46225024+ShermanIsCoding@users.noreply.github.com>
2020-07-25 15:16:51 +1000

    update book

--- app/manage.py
+++ app/manage.py
@@ -9,7 +9,7 @@ from app.main import create_app, db
 from app.main.model import user
 from app import blueprint
 
-app = create_app(os.getenv('BOILEPLATE_ENV') or 'dev')
+app = create_app('dev')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
